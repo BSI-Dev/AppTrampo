32fa880fb3dd23e50f4ba02702a163f18bc023b7
Jay-Robot <jota.ribeiro.silva@gmail.com>
2018-09-21 18:22:46 -0300

    Criação tela inicial.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button,Alert} from 'react-native';
+import {Header, Container} from "native-base";
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +22,35 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Header style={styles.header}><Text style ={styles.textHeader}>Trampo</Text></Header>
+        <View style={styles.perfis}>
+        <Button 
+          large
+          color='darkgrey' 
+          title='Cliente'
+
+          onPress={() => {Alert.alert('You tapped the button!');}}> 
+            
+          </Button>
+          <Button
+            large
+            
+            color='darkgrey'  
+            title='Prestador' onPress={() => {
+            Alert.alert('You tapped the button!');
+            }}>
+            </Button> 
+        </View>
+        <View style={styles.container}>
+          
+            
+          
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      </Container>
     );
   }
 }
@@ -46,4 +72,22 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  textHeader:{
+    top: 10,
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 5,
+    fontSize: 30,
+  },
+  header:{
+    backgroundColor: 'green',
+  },
+  perfis:{
+    top: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding:10,
+    justifyContent: 'space-between',
+  }
+  
 });
