6a297274f141f59594c1a03425cd4a6423e27d28
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2018-09-22 13:53:34 -0300

    Mudanças estruturais no projeto

---
+++ src/app/screens/Login.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+
+export default class Login extends Component{
+    render(){
+        return(
+            <View>
+                <View>
+
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#3498db',
+    }
+});
