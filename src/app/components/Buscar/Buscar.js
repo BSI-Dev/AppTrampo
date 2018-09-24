1a5178960667a329b6ede0106a3d42f7878eb74e
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2018-09-24 09:31:16 -0300

    Término da primeira tela

--- src/app/components/Buscar/Buscar.js
+++ src/app/components/Buscar/Buscar.js
@@ -1,51 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button,Alert, Image} from 'react-native';
 import {Header, Container} from "native-base";
 
 
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <Container>
         <Header style={styles.header}><Text style ={styles.textHeader}>Trampo</Text></Header>
-        <View style={styles.perfis}>
-        <Button 
-          large
-          color='darkgrey' 
-          title='Cliente'
-          
-          onPress={() => {Alert.alert('You tapped the button!');}}> 
-            
-          </Button>
-          <Button
-            large
-            
-            color='darkgrey'  
-            title='Prestador' onPress={() => {
-            Alert.alert('You tapped the button!');
-            }}>
-            </Button> 
-        </View>
-        <View style={styles.containerImage}>
-          
-         <Image 
-         style = {styles.logo}
-         source={require('./assets/trampo.png')}
-         
-         />   
-          
-          
-        </View>
+        <Text>TESTE        </Text>
       </Container>
     );
   }
